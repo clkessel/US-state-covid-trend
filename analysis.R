a582471d0ff09581932a2c5de4c57c4c31860c79
clkessel <christopher.lee.kessel@gmail.com>
2023-03-19 12:29:59 -0500

    initial draft

--- analysis.R
+++ analysis.R
@@ -2,50 +2,47 @@
 
 library(dplyr)
 library(ggplot2)
-library(lubridate) #for working with dates
+library(lubridate)
 library(scales)
 
-#set working directory
+#set working directory, read data, and examine structure
 setwd(paste(getwd(),"/data",sep=""))
-
-#read data and examine structure
 covid_data <- read.csv('United_States_COVID-19_Cases_and_Deaths_by_State_over_Time_-_ARCHIVED.csv')
 str(covid_data)
-head(covid_data)
 
 #examine state column...how many unique state values are there (looking for 50 but there are 60)
 covid_data %>% select(state) %>% unique() %>% count()
-#remove non-US state abbreviations and re-check totals
+#remove non-US state abbreviations
 covid_data <- covid_data %>% filter(!state %in% c("RMI", "PR", "VI", "RMI", "MP", "DC", "GU", "FSM", "AS", "PW"))
-covid_data %>% select(state) %>% unique() %>% count()
 #incorporate NYC values into NY state values
 covid_data$state[covid_data$state == "NYC"] <- "NY"
+#recheck total...should be 50 now
 covid_data %>% select(state) %>% unique() %>% count()
 
-#data wrangling
-#examine new_case data field...after removing the non-US states, there are 32 entries with new_case values less than 0!
-covid_data %>% filter(new_case < 0)
-
-##add month-year column
-##drop unnecessary columns
-#convert submission_date to date format
-str(covid_data$submission_date)
+#submission_date column was 'chr' before, format to 'date' and re-check
 covid_data$submission_date <- mdy(covid_data$submission_date)
-####need to convert to date 
+str(covid_data$submission_date)
 
-#check data frame structure
+#basic line plot, no state data
+covid_data_line <-covid_data %>% group_by(format(submission_date, "%Y-%m")) %>% summarize(monthly_total=sum(new_case))
+str(covid_data_line)
+colnames(covid_data_line) <- c("month", "total")
+covid_data_line %>%  ggplot(aes(x=month, y=total, group=1)) + ggtitle("U.S. Covid new case totals by month (Jan 2020 - Oct 2022)") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none") + 
+  scale_y_continuous(labels = comma) + geom_line()
 
+#basic line plot with each state a different color
+covid_data_lines <-covid_data %>% group_by(format(submission_date, "%Y-%m"), state) %>% summarize(monthly_total=sum(new_case))
+colnames(covid_data_lines) <- c("month", "state", "total")
+covid_data_lines %>%  ggplot(aes(x=month, y=total, color = state, group=state)) + ggtitle("U.S. Covid new case totals by month by state (Jan 2020 - Oct 2022)") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "right") + 
+  scale_y_continuous(labels = comma) + geom_line()
 
-#display visual
-covid_data %>% filter(new_case >= 0) %>% ggplot(aes(x=format(submission_date, "%Y-%m"), y=state, fill=sqrt(new_case))) +
+#tile plot with 2 x vertical intercepts
+covid_data %>% filter(new_case >= 0) %>%  ggplot(aes(x=format(submission_date, "%Y-%m"), y=state, fill=sqrt(new_case))) +
   geom_tile(color = "black") + 
-  scale_x_discrete(position = "top") + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
-  guides(fill = guide_colourbar(barwidth = 0.5, barheight = 20, title="Sqrt New Cases")) +
-  xlab("") + ylab("") +
-  scale_fill_gradient(trans = "sqrt", labels = label_comma()) + 
-  scale_size(guide = guide_legend(direction = "horizontal"))
-#scale_fill_gradientn(trans = "sqrt", colors = hcl.colors(20, "RdYlGn")) +
-#guides(fill=guide_legend(title="New Cases (sqrt)")) +
-
-
+  scale_x_discrete(position = "top") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none") + 
+  guides(fill = guide_colourbar(barwidth = 20, barheight = .5)) +
+  xlab("") + ylab("") + ggtitle("U.S. Covid New Case by State (Jan 2020 - Oct 2022)") +
+  scale_fill_gradient(low = "#000099", high = "#FFFFFF") + geom_vline(xintercept = c("2020-12", "2022-01"), color = "red")
